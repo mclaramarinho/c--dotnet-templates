2207a0104363dc81075b2c221273493438a4d0a4
Clara Marinho <119897667+mclaramarinho@users.noreply.github.com>
2024-03-11 22:15:58 -0300

    Update template2.cs
    has optional attrs

--- template2.cs
+++ template2.cs
@@ -24,18 +24,3 @@ public class MinhaClasse {
     }
 }
 
-class Program {
-    static void Main(string[] args) {
-        // Invocando o construtor com todos os atributos
-        MinhaClasse objeto1 = new MinhaClasse(10, "texto", 5.5, true);
-        Console.WriteLine("Objeto 1:");
-        objeto1.ExibirValores();
-
-        Console.WriteLine();
-
-        // Invocando o construtor sem os atributos opcionais
-        MinhaClasse objeto2 = new MinhaClasse(20, "outro texto");
-        Console.WriteLine("Objeto 2:");
-        objeto2.ExibirValores();
-    }
-}
